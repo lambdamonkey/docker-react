089232370eaf960a73d70f6cb0e2d65b4f403d4a
Evgeniy Smirniy <smirniyhr@gmail.com>
2019-05-25 20:12:09 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          So that's start now
+          So that's a change now
         </p>
         <a
           className="App-link"
